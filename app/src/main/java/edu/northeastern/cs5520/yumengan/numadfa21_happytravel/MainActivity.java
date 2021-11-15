bf141711bbc471be7f743d1ca8da7484e12c5332
Yumeng An <an.yum@northeastern.edu>
2021-11-14 20:30:56 -0800

    Add spotlessApply and also a popup menu

--- app/src/main/java/edu/northeastern/cs5520/yumengan/numadfa21_happytravel/MainActivity.java
+++ app/src/main/java/edu/northeastern/cs5520/yumengan/numadfa21_happytravel/MainActivity.java
@@ -1,9 +1,22 @@
 package edu.northeastern.cs5520.yumengan.numadfa21_happytravel;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.icu.util.Measure;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
@@ -27,5 +40,19 @@ public class MainActivity extends AppCompatActivity {
             // Log.v(TAG, "search bar!");
             Toast.makeText(this, "Searched!", Toast.LENGTH_LONG).show();
         });
+    }
+
+    public void showPopup(View view) {
+        View showView = LayoutInflater.from(this).inflate(R.layout.main_popup, null);
+        ImageButton popupButton = findViewById(R.id.main_menu_button);
+
+        PopupWindow popupWindow = new PopupWindow(showView,
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT,
+                true);
+        popupWindow.setContentView(showView);
+        showView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+
+        popupWindow.showAsDropDown(popupButton, 0,
+                -popupButton.getHeight() - showView.getMeasuredHeight());
     }
 }
